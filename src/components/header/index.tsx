4bcabdf4f59eb3c577cb43172aa14da1f9a05d84
CloudStar2077 <vladimir.ivic@gmx.de>
2026-07-07 23:51:19 +0200

    add dino logo to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,9 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
+        <a href="/Docusaurus/" className={styles.logo}>
+          <img src="/Docusaurus/img/docusaurus.png" alt="Home" className={styles.logoImg} />
+        </a>
         <nav className={styles.nav}>
           <a href="#about">About me</a>
           <a href="#skills">My skills</a>
